a74631d624c09c178b42f255f6cb359c36c9c8fd
anas138 <83574472+anas138@users.noreply.github.com>
2023-03-05 03:39:07 +0500

    get request

--- backend/Routers/router.js
+++ backend/Routers/router.js
@@ -4,7 +4,9 @@ const {Email} = require("../handler/emailHandler")
 
 const router = express.Router()
 
-
+router.get("/",(req,res)=>{
+    res.send("hello backend")
+})
 router.post("/users",addUsers)
 router.post('/login',login)
 router.get('/users',getAllUsers)
